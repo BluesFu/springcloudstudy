a0c97318e3a413e459f689bffa2b1172edff2449
BluesFu <siyangfu1314@gmail.com>
2020-08-08 00:55:40 +0800

    hystrix study

---
+++ cloud-provider-hystrix-payment8001/src/main/java/top/blues/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,15 @@
+package top.blues.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Siyang Fu
+ * @Date: 8/8/20 12:12 AM
+ */
+@SpringBootApplication
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/top/blues/springcloud/controller/PaymentController.java
@@ -0,0 +1,39 @@
+package top.blues.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jmx.export.naming.IdentityNamingStrategy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import top.blues.springcloud.service.PaymentService;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Siyang Fu
+ * @Date: 8/8/20 12:22 AM
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("$service.prot")
+    private String servicePort;
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+       String result=paymentService.paymentInfo_OK(id);
+       log.info("*****result"+result);
+       return result;
+    }
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id")Integer id){
+        String result=paymentService.paymentInfo_TimeOut(id);
+        log.info(">>>>>result"+result);
+        return result;
+    }
+}
