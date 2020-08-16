4d51032591df868fce2157c3df284fdd1412e9f9
BluesFu <siyangfu1314@gmail.com>
2020-08-17 00:25:41 +0800

    hystrix and HystrixDashboard

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 n%k= index
 
 ```
+##dashborad js 报错解决
+ - https://blog.csdn.net/oyc619491800/article/details/107420176
+ 修改js文件中
+ ```
+将上面的改成下面的即可加载出dashboard 表表盘 
+$(window).load(function() {    
+$(window).on('load', function() {  
+ ```
---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/top/blues/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package top.blues.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: Siyang Fu
+ * @Date: 8/16/20 10:45 PM
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
